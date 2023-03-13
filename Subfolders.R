9f8e9fd740994cd3654639e01c3dd95d013efa68
martie <andres-martinez@uiowa.edu>
2023-03-13 11:46:33 -0500

    final version. Include how to move files and delete them from original folder

--- Subfolders.R
+++ Subfolders.R
@@ -1,10 +1,14 @@
 # Create subfolder within the current working directory
-
 dir.create("Data")
 dir.create("R")
-dir.create("R/")
 dir.create("Output")
 dir.create("Output/Data")
 dir.create("Output/Data/csv")
 dir.create("Output/Data/excel")
 dir.create("Output/Plots")
+
+# Move files
+file.copy(from = "data.xlsx",
+          to = "Data/data.xlsx")
+# Delete files
+file.remove(from = "data.xlsx")
