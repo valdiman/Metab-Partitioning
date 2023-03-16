7882625659e84644d24163affc430c7a73959943
martie <andres-martinez@uiowa.edu>
2023-03-16 12:18:58 -0500

    Include physical-chemical data in the code and reformat a few lines

--- R/MetabPartitioning.R
+++ R/MetabPartitioning.R
@@ -2,34 +2,66 @@
 # 24 hr exposure time
 
 # Install packages
-install.packages("readxl")
-install.packages("gridExtra")
-install.packages("ggplot2")
-install.packages("extrafont")
+{
+  install.packages("gridExtra")
+  install.packages("ggplot2")
+  install.packages("extrafont")
+}
 
 # load libraries
 {
-  library(readxl)
   library(reshape2)
   library(ggplot2)
   library(extrafont) 
 }
 
+# Physical-chemical Properties --------------------------------------------
+# Create matrix to storage physical-chemical properties
+pc <- matrix(NA, nrow = 12, ncol = 5)
+# Add column names
+colnames(pc) <- c('congener', 'logKlipid/water', 'logKprotein/water',
+                  'logKair/water', 'dUaw')
+# Add row names
+pc[, 1] <- c("PCB3", "4-OH-PCB3", "4-OH-PCB3 sulfate", "PCB11",
+             "4-OH-PCB11", "4-OH-PCB11 sulfate", "PCB25",
+             "4-OH-PCB25", "4-OH-PCB25 sulfate", "PCB52",
+             "4-OH-PCB52", "4-OH-PCB52 sulfate")
 
-# Read data ---------------------------------------------------------------
+# Add physical-chemical properties
+# SMILES was used to determine OH-PCBs and OH-PCB sulfates
+# Reference:
+# Ulrich, N., Endo, S., Brown, T.N., Watanabe, N., Bronner, G., Abraham, M.H.,
+# Goss, K.-U., UFZ-LSER database v 3.2.1 [Internet], Leipzig, Germany,
+# Helmholtz Centre for Environmental Research-UFZ. 2017 [accessed on 16.03.2023].
+# Available from http://www.ufz.de/lserd
 
-# Read data.xlsx
-d <- data.frame(read_excel("Data/data.xlsx", sheet = "dataV2",
-                           col_names = TRUE, col_types = NULL))
+# logKlipid/water
+pc[, 2] <- c(5.05, 3.19, -0.33, 5.60, 4.85, 0.35, 6.06, 5.39, 0.88,
+             6.50, 5.22, 1.44)
+# logKprotein/water
+pc[, 3] <- c(3.40, 3.02, 0.70, 3.84, 3.53, 1.21, 4.22, 4.36, 1.65,
+             4.58, 4.34, 2.09)
+# logKair/water
+pc[, 4] <- c(-1.95, -6.13, -13.42, -1.93, -4.57, -13.48, -1.89,
+             -4.64, -13.55, -1.88, -5.59, -13.39)
+# dUaw (no available data for OH-PCBs and OH-PCB sulfates)
+pc[, 5] <- c(48734.74, 0.00, 0.00, 51662.48, 0.00, 0.00, 53590.22,
+             0.00, 0.00, 55517.96, 0.00, 0.00)
+# Transform pc matrix into data.frame
+pc <- as.data.frame(pc)
+# Transform values into numeric values
+pc$`logKlipid/water` <- as.numeric(pc$`logKlipid/water`)
+pc$`logKprotein/water` <- as.numeric(pc$`logKprotein/water`)
+pc$`logKair/water` <- as.numeric(pc$`logKair/water`)
+pc$dUaw <- as.numeric(pc$dUaw)
 
 # Calculations ------------------------------------------------------------
-
 # Name parameters
-congener <- d$congener
-logKa.w <- d$logKair.water
-dUaw <- d$dUaw
-logKlip.w <- d$logKlipid.water
-logKpro.w <- d$logKprotein.water
+congener <- pc$congener
+logKa.w <- pc$`logKair/water`
+logKlip.w <- pc$`logKlipid/water`
+logKpro.w <- pc$`logKprotein/water`
+dUaw <- pc$dUaw
 
 #Fixed parameters
 # 24-well plate https://www.corning.com/catalog/cls/documents/drawings/MD_Microplate_Dimension_Sheets_Multiple_Well.pdf
@@ -45,18 +77,18 @@ logKpro.w <- d$logKprotein.water
 }
 
 # Function to calculate fractions
-fraction = function(logKa.w, dUaw, logKlip.w, logKpro.w) {
-  
+fraction = function(logKa.w, dUaw, logKlip.w, logKpro.w)
+{
   # Concentrations in the wells
   Cell <- 4*10^4 # # cell/well
   C.cell <- Cell/Vm # Concentration of cell per well #cell/Vm (L)
   C.lipid.cell <- 9.57*10^-5/10^9 # lipid content kg/#cell
   C.lipid.L <- C.lipid.cell*C.cell # kg/L
-  dlipids <- 0.905 # kg/L ask! ref: https://pubmed.ncbi.nlm.nih.gov/8148928/
+  dlipids <- 0.905 # kg/L Reference: https://pubmed.ncbi.nlm.nih.gov/8148928/
   C.lipid <- C.lipid.L/dlipids # Lipid concentration Llipid/lwater
   C.prot.cell.0 <- 1.18*10^-4/10^9 # prot content kg/#cell
   C.prot.L <- C.prot.cell.0*C.cell # kg/L
-  dprot <- 1.43 # kg/L ask! ref: https://pubmed.ncbi.nlm.nih.gov/10930825/
+  dprot <- 1.43 # kg/L Reference: https://pubmed.ncbi.nlm.nih.gov/10930825/
   C.prot <- C.prot.L/dprot # Lipid concentration Lprot/lwater
   C.water.cell <- 2.84*10^-6 # uL/cell
   V.water.cell <- C.water.cell*Cell/10^6 # L water inside cell
@@ -71,9 +103,7 @@ fraction = function(logKa.w, dUaw, logKlip.w, logKpro.w) {
   logKa.w.t <- log10(Ka.w.t)
   
   # Fraction calculation
-  #C.t <- C.dis*(1 + 10^(log.Klip.w)*C.lipid + 10^(log.Kpro.w)*C.prot+
-  #                10^(log.Kw.air)*Va/(Vm-V.water.cell)
-  den <- 1 + 10^(logKlip.w)*C.lipid + 10^(logKpro.w)*C.prot+
+  den <- 1 + 10^(logKlip.w)*C.lipid + 10^(logKpro.w)*C.prot +
     10^(logKa.w.t)*Va/(Vm-V.water.cell)
   f.dis <- 1/den
   f.dis.m <- f.dis*(Vm-V.water.cell)/Vm
@@ -91,7 +121,8 @@ fraction = function(logKa.w, dUaw, logKlip.w, logKpro.w) {
 
 num.congener <- length(congener)
 result <- NULL
-for (i in 1:num.congener) {
+for (i in 1:num.congener)
+{
   result <- rbind(result, fraction(logKa.w[i], dUaw[i],
                                    logKlip.w[i], logKpro.w[i]))
 }
@@ -101,6 +132,8 @@ names(final.result) <- c("congener", "fract.diss.m", "fract.diss.cell",
                          "fract.lip", "fract.prot", "fract.air",
                          "fract.cell", "fract.diss.cell.cell",
                          "fract.lip.cell", "fract.prot.cell")
+# Export results
+write.csv(final.result, file = "Output/Data/csv/Fractions.csv")
 
 # Plots -------------------------------------------------------------------
 
