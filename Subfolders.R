14d95ef306bcc952933deae1201bf2b698953d27
martie <andres-martinez@uiowa.edu>
2023-03-16 14:53:11 -0500

    Delete a few lines

--- Subfolders.R
+++ Subfolders.R
@@ -4,11 +4,6 @@ dir.create("R")
 dir.create("Output")
 dir.create("Output/Data")
 dir.create("Output/Data/csv")
-dir.create("Output/Data/excel")
 dir.create("Output/Plots")
 
-# Move files
-file.copy(from = "data.xlsx",
-          to = "Data/data.xlsx")
-# Delete files
-file.remove(from = "data.xlsx")
+
