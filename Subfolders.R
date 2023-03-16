96fd3778e9dd219f9a06a9b699a8198ff85c095b
martie <andres-martinez@uiowa.edu>
2023-03-16 15:07:28 -0500

    Update the code.

--- Subfolders.R
+++ Subfolders.R
@@ -1,9 +1,6 @@
 # Create subfolder within the current working directory
-dir.create("Data")
 dir.create("R")
 dir.create("Output")
 dir.create("Output/Data")
 dir.create("Output/Data/csv")
 dir.create("Output/Plots")
-
-
